e7135eb4eb8df8e198ad3b980d075b420fb307dd
brunoluciano <bruno.lucian0544@gmail.com>
2020-08-27 01:27:25 -0300

    Tentando desenvolver lógica para evitar números repetidos no grid

--- Assets/Scripts/GridScript.cs
+++ Assets/Scripts/GridScript.cs
@@ -17,11 +17,122 @@ public class GridScript : MonoBehaviour
 
                 box[i,j] = boxGrid;
 
-                GameObject text = box[i,j].transform.GetChild(0).gameObject;
-                text.GetComponent<Text>().text = (Random.Range(1, 10)).ToString();
+                // GameObject text = box[i,j].transform.GetChild(0).gameObject;
+                // text.GetComponent<Text>().text = (Random.Range(1, 10)).ToString();
+            }
+        }
+
+        for (int i = 0; i < box.GetLength(0); i++) {
+            ArrayList vetAux = new ArrayList(9) {1,2,3,4,5,6,7,8,9};
+
+            for (int j = 0; j < box.GetLength(1); j++) {
+                bool possuiNumeroLinha = true;
+                bool possuiNumeroColuna = true;
+
+
+                
+                // while (verifica)
+                // {
+                    int numIndice = (Random.Range(0, vetAux.Count));
+                    string numero = vetAux[numIndice].ToString();
+
+                    possuiNumeroLinha = verificaLinha(i,numero);
+                    possuiNumeroColuna = verificaColuna(j,numero);  
+
+                    Debug.Log("Linha: " + possuiNumeroLinha + " - Coluna: " + possuiNumeroColuna + "- Número: " + numero);
+
+                    if(possuiNumeroLinha == false && possuiNumeroColuna == false) {
+                        GameObject text = box[i,j].transform.GetChild(0).gameObject;
+                        text.GetComponent<Text>().text = numero;
+                        vetAux.RemoveAt(numIndice);
+                    } else {
+                        GameObject text = box[i,j].transform.GetChild(0).gameObject;
+                        text.GetComponent<Text>().text = "99";
+
+
+                        /////////// TESTE BRUNO /////////
+
+                        // string numeroRepetido;
+
+                        // if(!possuiNumeroLinha) {
+                        //     numeroRepetido = getNumRow(i,numero);
+                        // } else if(!possuiNumeroColuna) {
+                        //     numeroRepetido = getNumCol(j,numero);
+                        // }
+
+                        // // vetAux = vetAux.Where(val => val != numeroRepetido);
+                        
+                        // int indice = IntParseFast(numeroRepetido) - 1;
+                        // vetAux.RemoveAt(indice);
+
+                        // numIndice = (Random.Range(0, vetAux.Count));
+                        // numero = vetAux[numIndice].ToString();
+
+                        // text.GetComponent<Text>().text = numero;
+                    }
+
+                // }
+
             }
         }
     }
+
+    bool verificaLinha(int row, string numero) {
+        for(int i = 0; i < box.GetLength(0); i++) {
+            GameObject boxChild = box[row,i].transform.GetChild(0).gameObject;
+            string boxText = boxChild.GetComponent<Text>().text;
+            
+            if(boxText == numero) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool verificaColuna(int col, string numero) {
+        for (int j = 0; j < box.GetLength(1); j++) {
+            GameObject boxChild = box[j,col].transform.GetChild(0).gameObject;
+            string boxText = boxChild.GetComponent<Text>().text;
+
+            if(boxText == numero) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // string getNumRow(int row, string numero) {
+    //     for(int i = 0; i < box.GetLength(0); i++) {
+    //         GameObject boxChild = box[row,i].transform.GetChild(0).gameObject;
+    //         string boxText = boxChild.GetComponent<Text>().text;
+
+    //         if(boxText == numero) {
+    //             return boxText;
+    //         }
+    //     }
+    // }
+
+    // string getNumCol(int col, string numero) {
+    //     for (int j = 0; j < box.GetLength(1); j++) {
+    //         GameObject boxChild = box[j,col].transform.GetChild(0).gameObject;
+    //         string boxText = boxChild.GetComponent<Text>().text;
+
+    //         if(boxText == numero) {
+    //             return boxText;
+    //         }
+    //     }
+    // }
+
+    // int IntParseFast(string value)
+    //     {
+    //     int result = 0;
+    //     for (int i = 0; i < value.Length; i++)
+    //     {
+    //         char letter = value[i];
+    //         result = 10 * result + (letter - 48);
+    //     }
+    //     return result;
+    //  }
 
     // Update is called once per frame
     void Update()
